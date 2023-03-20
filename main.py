97b0f543783cefe39caa5295ca5ddbadf7c6e29c
Arsenii Kondratov <arsent.ru@gmail.com>
2023-03-20 09:55:52 +0300

    Update main.py
    Починил проблему с временем, добавил комментарии

--- main.py
+++ main.py
@@ -9,25 +9,47 @@ def text_tickle(text,
                 width=100, height=100,
                 filename="text.mp4",
                 duration=3.0):
+    """
+    text - текст строки
+    text_color - цвет текста доступные варианты смотрите в TextClip.list('color')
+    width - ширина
+    height - высота
+    filename - имя файла
+    duration - продолжительность видео
+    """
     font = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", 30)
     size = font.getlength(text)
     print(size)
 
     def position(t):
-        return (width / 2 + (-(size / duration) * t), "center")
+        """
+        Вспомогательная функция
+        :param t:
+        принимает время в формате double
+        :return:
+        Возвращает позицию текста - tuple с позициями по X и Y в зависимости от времени
 
-    add_text = TextClip(text, fontsize=30, color=text_color, font="Arial").set_position(position).set_duration(3.0)
+        """
+        return width / 2 + (-(size / duration) * t), "center"
+    #создаём объект с текстом, указывает в позиции вспомогательную функцию которая расчитывает позицию по секундам
+    add_text = TextClip(text, fontsize=30, color=text_color, font="Arial") \
+        .set_position(position) \
+        .set_duration(duration)
 
+    # создаём сгруппированное видео из текста
     final = CompositeVideoClip([add_text], size=(width, height), bg_color=background_color)
     final.duration = duration
     final.fps = 60
     final.write_videofile(filename)
 
-
+#доступные цвета текста
+print("available text colors:")
 for i in TextClip.list('color'):
     print(i)
-text_tickle("Size of the picture in pixels. Can be auto-set if method=’label’, but mandatory if method=’caption’. the height can be None, it will then be auto-determined.",
-            filename="new.mp4",
-            text_color="VioletRed3",
-            background_color=(175, 25, 75),
-            )
+
+# вызов функции
+text_tickle(
+    "очень длинный текст",
+    filename="new.mp4",
+    text_color="SteelBlue1"
+    )
